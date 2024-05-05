2a98f95c3c8e1d45b0a858301831f90d5ffacdd9
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-05 23:42:02 +0530

    REST API Documentation

---
+++ src/main/java/com/apidoc/RestApiDocumentationApplication.java
@@ -0,0 +1,13 @@
+package com.apidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDocumentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apidoc/RestApiDocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
