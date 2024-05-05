504efabc4611e406e86b8fc1cda445879529839e
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-06 00:12:28 +0530

    db config added

--- src/main/java/com/apidoc/entity/Laptop.java
+++ src/main/java/com/apidoc/entity/Laptop.java
@@ -3,10 +3,24 @@
  */
 package com.apidoc.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author LENOVO
  *
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Laptop {
 	
 	private Long lapId;
--- src/main/java/com/apidoc/repo/LaptopRepo.java
+++ src/main/java/com/apidoc/repo/LaptopRepo.java
@@ -3,10 +3,14 @@
  */
 package com.apidoc.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apidoc.entity.Laptop;
+
 /**
  * @author LENOVO
  *
  */
-public interface LaptopRepo extends JPA {
+public interface LaptopRepo extends JpaRepository<Laptop, Long> {
 
 }
