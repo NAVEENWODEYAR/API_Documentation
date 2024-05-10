03a4fc1059d7216c4b7b0984279a5df9549fc7b3
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-10 23:46:48 +0530

    laptop dto defined

--- src/main/java/com/apidoc/dto/LaptopDto.java
+++ src/main/java/com/apidoc/dto/LaptopDto.java
@@ -3,10 +3,29 @@
  */
 package com.apidoc.dto;
 
+import com.apidoc.entity.Laptop;
+
+import jakarta.persistence.Column;
+
 /**
  * @author Naveen K Wodeyar
  * @date 06-05-2024
  */
 public class LaptopDto {
+	
+	public interface Create{}
+	public interface Update{}
+	
+	private Long lapId;
+	
+	private String lapModel;
+	
+	private String lapCompany;
+	
+	private Double lapPrice;
+	
+	private String lapReview;
+	
+	public static LaptopDto build(Laptop laptop) {}
 
 }
