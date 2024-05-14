de4c77b4979d539e362cbf0c4103b71a1b0565b3
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-14 14:51:21 +0530

    Service Impl layer added

---
+++ src/main/java/com/apidoc/service/impl/LaptopServiceImpl.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.apidoc.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.apidoc.service.LaptopService;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 14/05/2024
+ *
+ */
+@Service
+public class LaptopServiceImpl implements LaptopService{
+
+}
